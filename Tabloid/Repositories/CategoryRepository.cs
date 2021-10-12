3ba6a87f45428ddf49f8a525a173e17f885869e5
Jalon Duncan <duncanjalon@gmail.com>
2021-10-12 15:32:06 -0500

    server side written, writing client side

---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Tabloid.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository :BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration config) : base(config) { }
+        public List<Category> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, name
+                                      FROM Category
+                                      ORDER BY name";
+                    var reader = cmd.ExecuteReader();
+
+                    var categories = new List<Category>();
+
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return categories;
+                }
+            }
+        }
+    }
+}
