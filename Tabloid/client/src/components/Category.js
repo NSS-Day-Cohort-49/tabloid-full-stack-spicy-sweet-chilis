15c8ce5997d8c8230085ee2aa43d85ff7303e60e
Jalon Duncan <duncanjalon@gmail.com>
2021-10-12 15:44:37 -0500

    updated commit

---
+++ Tabloid/client/src/components/Category.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+const Category = ({ category }) => {
+    return (
+        <Card >
+            <p className="text-left px-2"></p>
+        </Card>
+    )
+}
