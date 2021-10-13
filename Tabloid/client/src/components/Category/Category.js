3aa46721264c46e6e532f0cb9ba09ccc197cb16c
Jalon Duncan <duncanjalon@gmail.com>
2021-10-13 15:50:09 -0500

    attempting to write logic for add, edit, and delete

--- Tabloid/client/src/components/Category/Category.js
+++ Tabloid/client/src/components/Category/Category.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
-
+import { Link } from "react-router-dom";
 const Category = ({ category }) => {
     return (
         <>
         <Card >
             <CardBody>
-                <h3>{category.name}</h3>
+                <h3 className="category_name"> {category.name}
+                <Link to="/category/edit"> Edit </Link>
+                <Link to="/category/delete"> Delete </Link>
+                </h3>
             </CardBody>
         </Card>
         </>
