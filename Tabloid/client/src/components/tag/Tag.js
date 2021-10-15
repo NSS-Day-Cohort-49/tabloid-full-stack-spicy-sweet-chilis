5bcb8d667b25eac1ab3500372f45c4c1be208767
Kelly Kessler <ksquaredkelly@gmail.com>
2021-10-15 11:45:25 -0500

    WIP TagForm

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -5,12 +5,14 @@ import { Card, CardBody, Button } from "reactstrap";
 const Tag = ({ tag }) => {
 
   return (
+    <>
     <Card>
       <CardBody>
           <strong>{tag.name}</strong>
           <Link to="/tags/form">Edit</Link>
       </CardBody>
     </Card>
+    </>
   );
 }
 export default Tag();
